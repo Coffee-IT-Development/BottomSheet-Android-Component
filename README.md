09c7f1e2a0fd789b361d7ff7beb45baf2f7d8d66
Daphne van der Veeke <vanderveeke@coffeeit.nl>
2022-08-31 22:37:50 +0200

    Open source

--- README.md
+++ README.md
@@ -1,21 +1,38 @@
-# Android Aroma BottomSheet
+[![Coffee IT - Android Aroma BottomSheet Component](https://coffeeit.nl/wp-content/uploads/2022/08/Aroma-emoji-picker-cover-android.png)](https://coffeeit.nl/)
 
-This document describes how to implement the Aroma BottomSheet component into an app
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/nl.coffeeit.aroma/bottomsheet/badge.svg)](https://maven-badges.herokuapp.com/Maven-Central/nl.coffeeit.aroma/bottomsheet)
+[![API](https://img.shields.io/badge/API-21%2B-brightgreen.svg?style=flat)](https://android-arsenal.com/api?level=21)
+[![License](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://github.com/Coffee-IT-Development/BottomSheet-Android-Component/blob/main/LICENSE)
+[![Mirror Repository](https://img.shields.io/badge/Mirror-Repository-9b34eb?style=flat-square)](https://github.com/Coffee-IT-Development/BottomSheet-Android-Component)
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-@CoffeeIT-blue.svg?style=flat-square)](https://linkedin.com/company/coffee-it)
+[![Facebook](https://img.shields.io/badge/Facebook-CoffeeITNL-blue.svg?style=flat-square)](https://www.facebook.com/CoffeeITNL/)
+[![Instagram](https://img.shields.io/badge/Instagram-CoffeeITNL-blue.svg?style=flat-square)](https://www.instagram.com/coffeeitnl/)
+[![Follow coffeeitnl on Twitter](https://img.shields.io/twitter/follow/coffeeitnl.svg?style=flat-square&logo=twitter)](https://twitter.com/coffeeitnl)
 
-# Installation
+# ☕️ Android Aroma BottomSheet
+
+The Android Aroma BottomSheet package provides a customisable BottomSheet. This README describes how to implement the BottomSheet into an app.
+
+This GitHub repository is a mirror, the official repository is hosted privately by Coffee IT.
+
+Created by [Coffee IT](https://coffeeit.nl/).
+
+<img src="docs/images/Demo.gif" width="300">
+
+# ⚡ Installation
 
 This component requires minimum SDK 21.
 
 Add the following to `build.gradle`:
 ```
 dependencies {
-    implementation "nl.coffeeit.aroma:bottomsheet:1.0.0"
+    implementation "nl.coffeeit.aroma:bottomsheet:1.0.1"
 }
 ```
 
-# Usage
+# 📖 Usage
 
-To use the component as a Composable, simply add it to your code as:
+To use the BottomSheet in a project, simply add it to your code like this:
 
 ```
 BottomSheetWithContent(
@@ -25,12 +42,12 @@ BottomSheetWithContent(
                     state.show()
                 }
             }) {
-                Text("This is behind the sheet")
+                Text("This goes behind the sheet")
             }
         },
         
         sheetContent = {
-            Text(text = "This is inside the sheet")
+            Text(text = "This goes inside the sheet")
         }
     )
 ```
@@ -38,24 +55,40 @@ Two parameters are required:
 
 `activityContentScope` holds the content that should be displayed initially, when the bottom sheet is hidden. It offers a `state` to toggle the visibility of the bottom sheet and a coroutine `scope`
 
-`sheetContent` represents a Composable that is the the content of the actual sheet
+`sheetContent` represents a Composable that is the the content of the actual sheet. XML can also be used here, since it can be inflated in a Composable.
 
-## Customisation
+# ⚙️ Customisation
+
 `BottomSheetWithContent` accepts the following optional parameters:
 
 - `backgroundColor` A `Color` object to set the background of the bottom sheet
 - `scrimColor` A `Color` object to set the color of the area behind the sheet
 - `width` The width of the bottom sheet (default is fill screen width)
-- `bottomPadding` Padding for the bottom of the sheet, to make a floating variant (default 0)
+- `bottomMargin` Margin for the bottom of the sheet, to make a floating variant (default 0)
 - `cornerShape` A `RoundedCornerShape` object to give the sheet rounded edges
-- `Accessory` Decides the accessory of the bottomsheet:
+- `accessory` Decides the accessory of the BottomSheet, can be:
   -- `NONE`
   -- `GRABBER`
   -- `CLOSE_BUTTON`
 
+# ✏️ Changelog
 
-# Change log
+The changelog can be found [here](https://github.com/Coffee-IT-Development/BottomSheet-Android-Component/blob/main/CHANGELOG.md).
 
-Version 1.0.0 *(01-07-2022)*
-----------------------------
-* Added BottomSheet component
+# 🔗 Related publications
+
+Look at our other repositories on our [GitHub account](https://github.com/orgs/Coffee-IT-Development/repositories).
+
+# 📧 Contact
+
+Do you have questions, ideas or need help? Send us an email at contact@coffeeit.nl.
+
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://global-uploads.webflow.com/605a171ee93af49275331843/623b23cdea80a92703e61b42_Logo_black_1.svg" width="100">
+  <source media="(prefers-color-scheme: light)" srcset="https://coffeeit.nl/wp-content/uploads/2016/09/logo_dark_small_new.png" width="100">
+  <img alt="The Coffee IT logo" src="https://coffeeit.nl/wp-content/uploads/2016/09/logo_dark_small_new.png">
+</picture>
+
+# ⚠️ License
+
+Android Aroma BottomSheet is licensed under the terms of the [MIT Open Source license](https://github.com/Coffee-IT-Development/BottomSheet-Android-Component/blob/main/LICENSE).
---
+++ sample/src/main/java/nl/coffeeit/sample/MainActivity.kt
@@ -0,0 +1,292 @@
+/*
+ * Created by Coffee IT
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 Coffee IT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package nl.coffeeit.sample
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.ajalt.colormath.model.HSV
+import com.github.ajalt.colormath.model.RGB
+import com.godaddy.android.colorpicker.harmony.ColorHarmonyMode
+import com.godaddy.android.colorpicker.harmony.HarmonyColorPicker
+import kotlinx.coroutines.launch
+import nl.coffeeit.aroma.bottomsheet.Accessory
+import nl.coffeeit.aroma.bottomsheet.BottomSheetWithContent
+import java.math.BigDecimal
+import kotlin.math.roundToInt
+
+const val DEFAULT_CORNER_RADIUS = 16f
+const val DEFAULT_SCRIM_COLOR = 0x99B0B0B3
+const val DEFAULT_SCRIM_COLOR_ALPHA = 0.7
+const val DEFAULT_SCRIM_COLOR_BLUE = 176
+const val DEFAULT_SCRIM_COLOR_GREEN = 176
+const val DEFAULT_SCRIM_COLOR_RED = 153
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComponentsDemoScreen()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun ComponentsDemoScreen(
+) {
+    var bottomSheetAccessory by remember { mutableStateOf(Accessory.NONE) }
+    var bottomSheetBottomMargin by remember { mutableStateOf(0f) }
+    var bottomSheetRgbColor by remember {
+        mutableStateOf(
+            RGB(
+                DEFAULT_SCRIM_COLOR_RED,
+                DEFAULT_SCRIM_COLOR_GREEN,
+                DEFAULT_SCRIM_COLOR_BLUE,
+                DEFAULT_SCRIM_COLOR_ALPHA
+            )
+        )
+    }
+    var bottomSheetRoundedCornerSize by remember { mutableStateOf(DEFAULT_CORNER_RADIUS) }
+    var bottomSheetScrimColor by remember { mutableStateOf(Color(DEFAULT_SCRIM_COLOR)) }
+    var bottomSheetScrimColorAlpha by remember { mutableStateOf(DEFAULT_SCRIM_COLOR_ALPHA) }
+    var bottomSheetWidth by remember { mutableStateOf(0f) }
+
+    val accessoryItems = listOf(Accessory.NONE, Accessory.GRABBER, Accessory.CLOSE_BUTTON)
+    val selectedAccessory = remember { mutableStateOf(Accessory.NONE) }
+    val isAccessorySelected: (Accessory) -> Boolean = { selectedAccessory.value == it }
+    val onAccessoryStateChange: (Accessory) -> Unit = { selectedAccessory.value = it }
+
+    BottomSheetWithContent(
+        { state, scope ->
+            Column {
+                Spacer(modifier = Modifier.height(48.dp))
+
+                Column(
+                    Modifier
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .verticalScroll(rememberScrollState())
+                    ) {
+
+                        Column {
+                            Text(text = "Corner radius: ${bottomSheetRoundedCornerSize.toInt()}dp")
+
+                            Slider(value = bottomSheetRoundedCornerSize, onValueChange = { value ->
+                                val roundedValue = value.roundToInt().toFloat()
+                                bottomSheetRoundedCornerSize = roundedValue
+                            }, valueRange = 0f..72f)
+
+                            Spacer(modifier = Modifier.width(32.dp))
+
+                            Text(text = "Width: ${bottomSheetWidth.toInt()}dp (enter 0dp for full width)")
+
+                            Slider(value = bottomSheetWidth, onValueChange = { value ->
+                                val roundedValue = value.roundToInt().toFloat()
+                                bottomSheetWidth = roundedValue
+                            }, valueRange = 0f..250f)
+
+                            Spacer(modifier = Modifier.width(32.dp))
+
+                            Text(text = "Bottom margin: ${bottomSheetBottomMargin.toInt()}dp")
+
+                            Slider(value = bottomSheetBottomMargin, onValueChange = { value ->
+                                val roundedValue = value.roundToInt().toFloat()
+                                bottomSheetBottomMargin = roundedValue
+                            }, valueRange = 0f..250f)
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Column {
+                            Text(
+                                text = "Accessory:"
+                            )
+
+                            Row(
+                                Modifier
+                                    .wrapContentSize()
+                                    .padding(top = 8.dp)
+                            ) {
+                                accessoryItems.forEach { item ->
+                                    Row(
+                                        verticalAlignment = Alignment.CenterVertically,
+                                        modifier = Modifier
+                                            .selectable(
+                                                selected = isAccessorySelected(item),
+                                                onClick = {
+                                                    onAccessoryStateChange(item)
+                                                    bottomSheetAccessory = item
+                                                },
+                                                role = Role.RadioButton
+                                            )
+                                            .padding(end = 4.dp)
+                                            .wrapContentSize()
+                                    ) {
+                                        RadioButton(
+                                            selected = isAccessorySelected(item),
+                                            onClick = null
+                                        )
+
+                                        Spacer(modifier = Modifier.width(4.dp))
+
+                                        Text(
+                                            text = item.displayName
+                                        )
+                                    }
+                                }
+                            }
+
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Text(text = "Scrim alpha: $bottomSheetScrimColorAlpha")
+
+                        Slider(
+                            value = bottomSheetScrimColorAlpha.toFloat(),
+                            onValueChange = { value ->
+                                var valueFormatted = BigDecimal(value.toDouble())
+                                valueFormatted = valueFormatted.setScale(2, BigDecimal.ROUND_UP)
+                                bottomSheetScrimColorAlpha = valueFormatted.toDouble()
+                                bottomSheetRgbColor = RGB(
+                                    bottomSheetRgbColor.r,
+                                    bottomSheetRgbColor.g,
+                                    bottomSheetRgbColor.b,
+                                    bottomSheetScrimColorAlpha
+                                )
+                                bottomSheetScrimColor = Color(
+                                    bottomSheetRgbColor.redInt,
+                                    bottomSheetRgbColor.greenInt,
+                                    bottomSheetRgbColor.blueInt,
+                                    bottomSheetRgbColor.alphaInt
+                                )
+                            },
+                            valueRange = 0f..1f
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Text(text = "Scrim color and brightness:")
+
+                        HarmonyColorPicker(
+                            harmonyMode = ColorHarmonyMode.SHADES,
+                            modifier = Modifier
+                                .size(275.dp)
+                                .align(Alignment.CenterHorizontally),
+                            color = Color(DEFAULT_SCRIM_COLOR),
+                            onColorChanged = { hsvColor ->
+                                bottomSheetRgbColor = HSV(
+                                    hsvColor.hue,
+                                    hsvColor.saturation,
+                                    hsvColor.value,
+                                    bottomSheetScrimColorAlpha
+                                ).toSRGB()
+                                bottomSheetScrimColor = Color(
+                                    bottomSheetRgbColor.redInt,
+                                    bottomSheetRgbColor.greenInt,
+                                    bottomSheetRgbColor.blueInt,
+                                    bottomSheetRgbColor.alphaInt
+                                )
+                            })
+
+                        Button(modifier = Modifier
+                            .fillMaxWidth(),
+                            onClick = {
+                                scope.launch {
+                                    state.show()
+                                }
+                            }
+                        ) {
+                            Text(text = "Show BottomSheet")
+                        }
+                    }
+                }
+
+                BackHandler(
+                    enabled = (state.currentValue != ModalBottomSheetValue.Hidden),
+                    onBack = {
+                        scope.launch { state.hide() }
+                    }
+                )
+            }
+        },
+        contentComposable = {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+            ) {
+
+                Image(
+                    modifier = Modifier
+                        .width(231.dp)
+                        .wrapContentHeight()
+                        .align(Alignment.CenterHorizontally)
+                        .padding(horizontal = 16.dp)
+                        .padding(vertical = 72.dp),
+                    painter = painterResource(R.drawable.ill_aroma_logo),
+                    contentDescription = "Coffee IT's Aroma logo",
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+        },
+        width = bottomSheetWidth,
+        bottomMargin = bottomSheetBottomMargin,
+        accessory = bottomSheetAccessory,
+        cornerShape = RoundedCornerShape(bottomSheetRoundedCornerSize.dp),
+        scrimColor = bottomSheetScrimColor
+    )
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun ModalBottomSheetHolderPreview() {
+    ComponentsDemoScreen()
+}
